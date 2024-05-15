95082e272646d3ca9df1e7db344e406b5777f6f6
MagnusBoock <magnus.boock@gmail.com>
2024-05-15 16:29:03 +0200

    Implemented member functions for inherriting traits and mututating traits

--- python_skeleton/model.py
+++ python_skeleton/model.py
@@ -15,7 +15,7 @@ class Model:
             aggression : float, # 0.0 - 1.0, where 0.0:0.5 is cautious, 0.5:1.0 is aggressive
             betting_mean : float,
             betting_std : float, 
-            deceptiveness : float
+            deceptiveness : float # 0.0 - 1.0
         ):
         self.strength_threshold_bet = strength_threshold_bet
         self.strength_threshold_call = strength_threshold_call
@@ -24,6 +24,15 @@ class Model:
         self.betting_std = betting_std
         self.deceptiveness = deceptiveness
         self.betting_distribution = np.random.normal(loc=betting_mean, scale=betting_std)
+        self.locked_attributes = ["betting_distribution"]
+
+    def locked_attributes_append(self, attribute):
+        if attribute not in self.locked_attributes:
+            self.locked_attributes.append(attribute)
+
+    def locked_attributes_remove(self, attribute):
+        if attribute in self.locked_attributes:
+            self.locked_attributes.remove(attribute)
 
     def bet_amount(self, remaining_stack):
         """Pick a bet based on remaining stack and betting distribution"""
@@ -62,7 +71,7 @@ class Model:
         
         legal_actions = round_state.legal_actions()
         
-        # If the probability of winning is higher than the threshold for betting, then bet
+        # if the probability of winning is higher than the threshold for betting, then bet
         if RaiseAction in legal_actions and prob_win > self.strength_threshold_bet:
             return self.bet(prob_win)
         
@@ -73,10 +82,37 @@ class Model:
             return self.check()
         
         # Bluffing
-        # If deception is higher than a random number [0;1), then bluff
+        # if deception is higher than a random number [0;1), then bluff
         # deception ONLY applies to RaiseAction, as calling ~~ loosing money if you don't have a good hand
-        elif (RaiseAction in legal_actions) and self.deceptiveness > random.random():
+        elif (RaiseAction in legal_actions) and random.random() < self.deceptiveness:
             return self.bluff(prob_win)
         
         else:
             return self.fold()
+    
+    def inherit_traits(self, other, chance=0.05, volatility=0.1):
+        allowed_attributes = [attr_name for attr_name in vars(self) if attr_name not in self.locked_attributes]
+        for attr_name in allowed_attributes:
+            if random.random() < chance:
+                s_val = getattr(self, attr_name) # self attribute
+                o_val = getattr(other, attr_name) # other attribute
+                diff = s_val - o_val
+                s_val += diff * volatility
+                
+                # ensure that the value is within the bounds of the attribute
+                s_val = max(0, min(1, s_val))
+                
+                setattr(self, attr_name, s_val)
+    
+    def mutate(self, chance, volatility):
+        allowed_attributes = [attr_name for attr_name in vars(self) if attr_name not in self.locked_attributes]
+        for attr_name in allowed_attributes:
+            if random.random() < chance:
+                val = getattr(self, attr_name)
+                
+                mod = val * (1 + volatility)
+                mod *= 1 if random.random() < 0.5 else -1
+                
+                val = max(0, min(1, val + mod))
+                
+                setattr(self, attr_name, val)
